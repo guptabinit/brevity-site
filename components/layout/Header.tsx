7caaa0dc960fb5caf8be7ae3778cdf3d6b00c0c4
RachitReserk <trachit752@gmail.com>
2025-04-15 18:31:48 +0530

    Fixed navbar

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -22,7 +22,7 @@ export const Header = () => {
   }
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 w-full">
+    <header className="sticky top-0 z-50 w-full">
       <AnnouncementBar />
       <div
         className={`w-full transition-all duration-300 ${
